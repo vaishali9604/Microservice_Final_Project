e52a5aefb138af09c66a22d0bec205c0cbdf156f
Vaishali9604 <Vaishalim96@gmail.com>
2020-07-27 03:14:05 +0000

    Account project

---
+++ MT.OnlineRestaurant.AccountManagement/MT.OnlineRestuarant.DataLayer/ApplicationString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MT.OnlineRestuarant.DataLayer
+{
+   public class ApplicationString
+    {
+        // private string _Db = "Data Source=vs2019vm\\SQLEXPRESS;Initial Catalog=AccountManagement;Integrated Security=True;";
+        private string _Db = "Server=(localdb)\\v11.0;Database=AccountManagement;Trusted_Connection=True;MultipleActiveResultSets=true;";
+
+        public string DB
+        {
+            get
+            {
+                return _Db;
+            }
+            set
+            {
+                _Db = value;
+            }
+        }
+    }
+}
